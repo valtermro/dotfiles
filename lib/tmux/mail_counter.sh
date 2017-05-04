f1a4c6c0b9bd2e0022bac3db68174d2afc84565b
valtermro <valtermro@outlook.com>
2017-05-04 11:09:06 -0300

    (mutt) Store emails in /var/mail/$USER/<account>

--- lib/tmux/mail_counter.sh
+++ lib/tmux/mail_counter.sh
@@ -8,19 +8,18 @@ if [[ ! $(ps aux | grep $(which offlineimap)$) ]]; then
   exit 0
 fi
 
-# offlineimap is configured to store emails in directories named after the account
-# names and stored in `$X_VAR_HOME/mail`
-accounts=$(ls $X_VAR_HOME/mail/)
+# offlineimap is configured to store emails in directories named `/var/mail/$USER/<account>`
+accounts=$(ls /var/mail/$USER)
 
 # iterate over each account directory, read the `new` folder of each mailbox,
 # skipping those listed in `$ignored_mailboxes` and increment `$count` by each
 # file in there
 for account in $accounts; do
-  for folder in $(ls $X_VAR_HOME/mail/$account); do
+  for folder in $(ls /var/mail/$USER/$account); do
     if [[ "${ignored_mailboxes[@]}" =~ "${folder}" ]]; then
       continue
     fi
-    let count+=$(ls $X_VAR_HOME/mail/$account/$folder/new/* | wc -l)
+    let count+=$(ls /var/mail/$USER/$account/$folder/new/* | wc -l)
   done
 done
 
