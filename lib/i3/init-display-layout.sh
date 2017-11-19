c6c5828935d07b0c7393a182dcd09227f1b86265
valtermro <valtermro@outlook.com>
2017-11-19 21:14:06 -0200

    (i3) Change default monitors layout

--- lib/i3/init-display-layout.sh
+++ lib/i3/init-display-layout.sh
@@ -4,7 +4,9 @@ main_workspace=$1
 external_display=$(xrandr | grep '\<connected\>'| cut -d' ' -f1 | grep 'HDMI')
 
 if [[ $external_display ]]; then
-  $X_DOTFILES/bin/dlayout eright
+  $X_DOTFILES/bin/dlayout eleft
   i3-msg "workspace $main_workspace; move workspace to output $external_display"
-  i3-msg "restart"
+  # start a terminal here to make sure it opens in the main workspace
+  i3-msg 'exec --no-startup-id i3-sensible-terminal'
+  i3-msg 'restart'
 fi
